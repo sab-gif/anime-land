a67159c393735568a6cb47bb130652241692daff
saba <sabaurrehman11@gmail.com>
2025-08-26 09:56:06 +0200

    Initial commit: Spring Boot project generated

---
+++ src/main/java/com/explore/anime_land/AnimeLandApplication.java
@@ -0,0 +1,13 @@
+package com.explore.anime_land;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeLandApplication.class, args);
+	}
+
+}
