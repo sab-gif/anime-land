ccd09d5c9d962d2319d613ba4e03fe87a89f53ae
saba <sabaurrehman11@gmail.com>
2025-09-03 23:10:16 +0200

    chore: create AdminController placeholder with no implementation yet

--- src/main/java/com/explore/anime_land/users/AdminController.java
+++ src/main/java/com/explore/anime_land/users/AdminController.java
@@ -1,4 +1,16 @@
 package com.explore.anime_land.users;
 
-public class AdminController {
-}
+
+/*import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@PreAuthorize("hasRole('Admin')")
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController extends SecuredBaseController {
+
+    private final User
+}*/
