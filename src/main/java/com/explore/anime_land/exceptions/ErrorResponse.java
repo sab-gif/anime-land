eb85dd46f2a5b164b02c4defe8bae5820d488b22
saba <sabaurrehman11@gmail.com>
2025-09-04 00:24:42 +0200

    feat: add ErrorResponse class for structured API error details

--- src/main/java/com/explore/anime_land/exceptions/ErrorResponse.java
+++ src/main/java/com/explore/anime_land/exceptions/ErrorResponse.java
@@ -1,4 +1,26 @@
 package com.explore.anime_land.exceptions;
 
-public class ErrorResponse {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record  ErrorResponse
+    (@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    LocalDateTime timestamp,
+    int status,
+    String error,
+    Object message,
+    String path){
+
+    public ErrorResponse (HttpStatus status, Object message, HttpServletRequest req){
+            this(LocalDateTime.now(), status.value(), status.name(), message, String.valueOf(req.getRequestURI()));
+        }
+
+    public ErrorResponse (HttpStatus status, String error, Object message, HttpServletRequest req){
+            this(LocalDateTime.now(), status.value(), error, message, String.valueOf(req.getRequestURI()));
+        }
 }
