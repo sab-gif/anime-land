393f82d097b914e0be9ef0efcf613e0788e87851
saba <sabaurrehman11@gmail.com>
2025-09-03 22:42:07 +0200

    feat: create UserRepository interface for User entity CRUD operations

--- src/main/java/com/explore/anime_land/users/UserRepository.java
+++ src/main/java/com/explore/anime_land/users/UserRepository.java
@@ -1,4 +1,12 @@
 package com.explore.anime_land.users;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
