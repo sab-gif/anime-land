fd24f67327595a3aec254d812a1c86aec0652238
saba <sabaurrehman11@gmail.com>
2025-09-04 00:25:49 +0200

    feat: add EntityNotFoundException for handling missing entity errors

--- src/main/java/com/explore/anime_land/exceptions/EntityNotFoundException.java
+++ src/main/java/com/explore/anime_land/exceptions/EntityNotFoundException.java
@@ -1,7 +1,7 @@
 package com.explore.anime_land.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
-  public EntityNotFoundException(String message) {
-    super(message);
-  }
+    public EntityNotFoundException(String entityClass, String attributeName, String attributeValue) {
+        super(String.format("%s with %s %s not found", entityClass, attributeName, attributeValue));
+    }
 }
