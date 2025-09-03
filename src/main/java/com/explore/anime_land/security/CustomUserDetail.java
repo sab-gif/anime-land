091417cfeb8839d87d31fc571e80e5cce8be3d96
saba <sabaurrehman11@gmail.com>
2025-09-04 00:15:26 +0200

    feat: add CustomUserDetail for Spring Security user details integration

--- src/main/java/com/explore/anime_land/security/CustomUserDetail.java
+++ src/main/java/com/explore/anime_land/security/CustomUserDetail.java
@@ -8,6 +8,35 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class CostumUserDetail implements UserDetails {
+public class CustomUserDetail implements UserDetails {
+    private final User user;
+
+    public CustomUserDetail(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
 
 }
