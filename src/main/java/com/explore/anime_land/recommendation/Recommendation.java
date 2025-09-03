8659017ef35b5f48bc68199c6c6879b51d7d0126
saba <sabaurrehman11@gmail.com>
2025-09-04 00:32:05 +0200

    chore: create empty Recommendation class as placeholder for future implementation

--- src/main/java/com/explore/anime_land/recommendation/Recommendation.java
+++ src/main/java/com/explore/anime_land/recommendation/Recommendation.java
@@ -1,4 +1,20 @@
-package com.explore.anime_land.recomendation;
+package com.explore.anime_land.recommendation;
 
+import com.explore.anime_land.anime.Anime;
+import com.explore.anime_land.users.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "recommendations")
 public class Recommendation {
+    @ManyToOne
+     @JoinColumn(name ="anime_id")
+     private Anime anime;
+
+    @ManyToOne
+     @JoinColumn(name ="user_id")
+     private User author;
 }
