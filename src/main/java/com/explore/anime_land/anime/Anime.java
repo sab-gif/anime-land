e7ea978371fd57673b8569196623b23c386fedc5
saba <sabaurrehman11@gmail.com>
2025-09-05 03:21:57 +0200

    feat(entity): add Anime entity with JPA annotations
    - Added the Anime entity class with fields: id, title, genre, description, imageUrl, and averageRating
    - Configured JPA annotations for table mapping, primary key generation, and column details
    - Included Lombok annotations for boilerplate code reduction and equality/hashcode implementations

--- src/main/java/com/explore/anime_land/anime/Anime.java
+++ src/main/java/com/explore/anime_land/anime/Anime.java
@@ -8,14 +8,33 @@ import lombok.*;
 import java.util.List;
 
 @Entity
-@Table(name= "anime")
+@Table(name= "animes")
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(nullable = false, length = 1024)
+    private String description;
+
+    @Column(name = "image_Url")
+    private String imageUrl;
+
+    @Column(name = "average_rating")
+    private double averageRating;
+
     @ManyToOne
             @JoinColumn(name = "user_id")
             private User user;
