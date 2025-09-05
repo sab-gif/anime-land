3ce04790b1c13c6865eefd8c78eac347b5072f95
saba <sabaurrehman11@gmail.com>
2025-09-05 03:33:58 +0200

    feat(dto): add AnimeRequest record with validation and Swagger annotations

--- src/main/java/com/explore/anime_land/anime/dto/AnimeRequest.java
+++ src/main/java/com/explore/anime_land/anime/dto/AnimeRequest.java
@@ -1,4 +1,27 @@
 package com.explore.anime_land.anime.dto;
 
-public record AnimeRequest() {
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record AnimeRequest(
+        @Schema(description = "Title", example = "Jujutsu Kaisen", required = true)
+        @NotBlank(message = "Title is required")
+        @Size(min=5, max=15, message = "Title must be more than 5 characters and less than 15 characters")
+        String title,
+
+        @Schema(description = "genre", example = "Darkfantasy, action", required = true)
+        @NotBlank (message = "Genre is required")
+        @Size(min=8, max=20, message = "Genre must be more than 8 characters and less than 30 characters")
+        String genre,
+
+        @Schema(description = "Description", example = "Jujutsu Kaisen follows high school student Yuji Itadori, who becomes the host to a powerful demon named Sukuna after swallowing one of his fingers, and then joins a secret organization of Jujutsu Sorcerers to help control Sukuna's power and eliminate powerful Curses born from negative human emotions")
+        @Size(max=500, message = "Description must be less than 500 characters")
+        String description,
+
+        @Schema(description = "ImageUrl", example = "https://example.com/image.jpg")
+        @Pattern(message = "Invalid content type", regexp = "^(https?://.*\\.(png|jpg|jpeg|gif|svg))$")
+        String imageUrl
+) {
 }
