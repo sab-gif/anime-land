5d665fc1cca929f5e37f8ef9b9065511023a5515
saba <sabaurrehman11@gmail.com>
2025-09-06 16:36:39 +0200

    feat(recommendation): add Recommendation entity ID, anime, user, and datePosted fields and relations to User and Anime

--- src/main/java/com/explore/anime_land/recommendation/Recommendation.java
+++ src/main/java/com/explore/anime_land/recommendation/Recommendation.java
@@ -2,19 +2,26 @@ package com.explore.anime_land.recommendation;
 
 import com.explore.anime_land.anime.Anime;
 import com.explore.anime_land.users.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "recommendations")
 public class Recommendation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne
      @JoinColumn(name ="anime_id")
      private Anime anime;
 
     @ManyToOne
      @JoinColumn(name ="user_id")
-     private User author;
+     private User user;
+
+    @Column(name = "date_posted", nullable = false)
+    private LocalDateTime datePosted;
 }
